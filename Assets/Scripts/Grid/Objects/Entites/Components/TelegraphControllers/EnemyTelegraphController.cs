1f343afd684e4731ee438f6e62a7604e73ecbb9f
Oleksandr Kyivskyi <kievskiyalexandr@gmail.com>
2021-04-07 11:52:10 +0300

    Moved input setting and renamed inputsystem foler, changed enemy telegraph to use new input system

--- Assets/Scripts/Grid/Objects/Entites/Components/TelegraphControllers/EnemyTelegraphController.cs
+++ Assets/Scripts/Grid/Objects/Entites/Components/TelegraphControllers/EnemyTelegraphController.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace ShadowWithNoPast.Entities
 {
-    public class EnemyTelegraphController : BasicTelegraphController
+    public class EnemyTelegraphController : BasicTelegraphController, IPointerEnterHandler, IPointerExitHandler
     {
-        public void OnMouseEnter()
+        public void OnPointerEnter(PointerEventData eventData)
         {
             TelegraphAvailableMove(0.75f);
         }
 
-        public void OnMouseExit()
+        public void OnPointerExit(PointerEventData eventData)
         {
             ClearAvalableMoves();
         }
